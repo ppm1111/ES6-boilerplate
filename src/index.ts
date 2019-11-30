c44549e7b9f5cde99cbbf7403383d5531ae50e78
ppm1111 <ppqqbbdd1111@gmail.com>
2019-11-30 22:47:18 +0800

    starter

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import './scss/main.scss';
+
+const sayHello = () => {
+  /*eslint-disable no-console */
+  console.log('Allo! We are all setq!');
+  console.log('Arrow functions are working');
+};
+
+sayHello();
+
+for (var a = 10; a < 10; a++) {
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    entry: __dirname + '/src/index.js',
+    entry: __dirname + '/src/index.ts',
     output: {
         path: __dirname + '/dist',
         publicPath: '/dist/',
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }]
-    }
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                // 需要用到的 loader
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader',
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // 指定輸出位置
+            // [name] 為上方進入點設定的 "名稱"
+            filename: './css/[name].css',
+        }),
+    ],
 };
